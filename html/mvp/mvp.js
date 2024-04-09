42b7da1e12f49bc1ffe6f9ac5c5200ac97f640e5
p72losaj <55488505+p72losaj@users.noreply.github.com>
2024-04-09 12:43:16 +0200

    Update mvp.js

--- html/mvp/mvp.js
+++ html/mvp/mvp.js
@@ -5,8 +5,8 @@ function enviarCorreo() {
     const mensaje = document.getElementbyId('mensaje').value;
 
     // Construccion del link mailto
-    const mailtoLink = 'mailto:jlorenzosa@gmail.com?subject=Contacto%20de%20${nombre}&body=Nombre: ${nombre}&0AEmail: ${correo}%0AMensaje: ${mensaje}';
-
+    //const mailtoLink = 'mailto:jlorenzosa@gmail.com?subject=Contacto%20dede%20[TutorTech]&body=Nombre:%20[nombre]&0ACorreo%20electrónico:%20[correo]%0AMensaje:%20$[mensaje]';
+    const mailtoLink =  "mailto:jlorenzosa@gmail.com";
     // Redirigir usuario al link mailto
     window.open(mailtoLink);
 }
