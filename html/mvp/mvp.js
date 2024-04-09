909952e5b8332f412d9bd1907e579acae267dc14
p72losaj <55488505+p72losaj@users.noreply.github.com>
2024-04-09 12:45:04 +0200

    Update mvp.js

--- html/mvp/mvp.js
+++ html/mvp/mvp.js
@@ -1,12 +1,1 @@
-function enviarCorreo() {
-    // Obtener la información del formulario
-    const nombre = document.getElementbyId('nombreCompleto').value;
-    const correo = document.getElementbyId('correo').value;
-    const mensaje = document.getElementbyId('mensaje').value;
 
-    // Construccion del link mailto
-    //const mailtoLink = 'mailto:jlorenzosa@gmail.com?subject=Contacto%20dede%20[TutorTech]&body=Nombre:%20[nombre]&0ACorreo%20electrónico:%20[correo]%0AMensaje:%20$[mensaje]';
-    const mailtoLink =  "mailto:jlorenzosa@gmail.com";
-    // Redirigir usuario al link mailto
-    window.open(mailtoLink);
-}
